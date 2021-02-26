bf136b9793d1a693f055c8684cdd23732109ab24
AndrisZo <andris.zonies@me.com>
2021-02-25 22:00:47 -0500

    Finished state machine portion

---
+++ lib/multibulls/multibulls.ex
@@ -0,0 +1,235 @@
+_ = """
+{
+  guesses: [
+      [Username, [1, 2, 3, 4], bulls, cows],
+      [Username, [1, 2, 3, 4], bulls, cows],
+      [Username, [1, 2, 3, 4], bulls, cows],
+      [Username, [1, 2, 3, 4], bulls, cows]
+  ],
+  secret: [1, 2, 3, 4],
+  currentguesses: {
+      username : [currentguess],
+      username : [currentguess],
+      username : [currentguess],
+  }
+  passed: [Username, Username],
+  winners: [Username, Username],
+  playerswinloss: {
+      Username : [win, loss],
+      Username : [win, loss],
+  }
+  playersready: [Username, Username]
+  turnnumber: integer
+  setup: boolean
+}
+
+Calls the user can make to the server:
+- "makeguess"
+- "pass"
+- "login"
+- "ready-up"
+- "ready-down"
+- "becomeplayer"
+- "becomeobserver"
+- "exitgame"
+- "joingame"
+- "ping"
+"""
+
+defmodule Multibulls.Game do
+  import Enum
+
+  # Get a new, blank game object
+  def new() do
+    %{
+      guesses: [],
+      secret: random_digits(),
+      currentguesses: %{},
+      passed: [],
+      winners: [],
+      playerswinloss: %{},
+      playersready: [],
+      turnumber: 0,
+      setup: true
+    }
+  end
+
+  def startnewgame(game) do
+    game
+    |> Map.put(:currentguesses, %{})
+    |> Map.put(:guesses, [])
+    |> Map.put(:passed, [])
+    |> Map.put(:playersready, [])
+    |> Map.put(:secret, random_digits())
+    |> Map.put(:setup, false)
+    |> Map.put(:turnnumber, game.turnnumber + 1)
+    |> Map.put(:winners, [])
+  end
+
+  # Set the user's current guess to the given guess
+  def makeguess(gamestate, name, guess) do
+    if isValidGuess(guess) do
+      gamestate = Map.put(gamestate, :currentguesses, Map.put(gamestate.currentguesses, name, guess))
+      Map.put(gamestate, :passed, List.delete(gamestate.passed, name))
+      # if allplayersguessed(gamestate) do
+        # pushguesses(gamestate)
+      # else do
+        # gamestate
+    end
+  end
+
+  # Is the given guess a list of 4 unique numbers?
+  def isValidGuess(guess) do
+    Enum.count(guess) == 4 and Enum.count(Enum.uniq(guess)) == 4
+  end
+
+  # Have all the players either made a guess or passed?
+  def allplayersguessed(gamestate) do
+    Enum.sort(Enum.concat(Map.keys(gamestate.currentguesses), gamestate.passed)) == Enum.sort(Map.keys(gamestate.playerswinloss))
+  end
+
+  # Push any guesses that have been made to the list of guesses. Anyone that doesn't have a guess there
+  # is assumed to have passed.
+  # Clear out the map of current guesses
+  # Reset the list of people that have passed
+  # If someone won the game, add them to the winners and reset to setup state.
+  def pushguesses(gamestate) do
+    newguesses = all_cows_bulls(extractguesses(gamestate.currentguesses), gamestate.secret)
+    state = Map.put(gamestate, :guesses, Enum.concat(gamestate.guesses, newguesses))
+    |> Map.put(:currentguesses, %{})
+    |> Map.put(:passed, [])
+    |> Map.put(:turnnumber, gamestate.turnnumber + 1)
+    winners = anywinners(newguesses)
+    if Enum.empty?(winners) do
+      state
+    else
+      state
+      |> Map.put(:winners, winners)
+      |> Map.put(:setup, true)
+      |> Map.put(:playersready, [])
+      |> Map.put(:playerswinloss, updatewinloss(gamestate.playerswinloss, winners))
+    end
+  end
+
+  def updatewinloss(winloss, winners) do
+    cond do
+      Enum.empty?(winners) -> winloss
+      true -> updatewinloss(winloss, Map.keys(winloss), winners)
+    end
+  end
+
+  def updatewinloss(winloss, winlosskeys, winners) do
+    cond do
+      Enum.empty?(winlosskeys) -> winloss
+      Enum.count(winners, fn x -> x == hd(winlosskeys) end) > 0 ->
+        updatewinloss(Map.put(winloss, hd(winlosskeys), addwin(winloss[hd(winlosskeys)])), tl(winlosskeys), winners)
+      true -> updatewinloss(Map.put(winloss, hd(winlosskeys), addloss(winloss[hd(winlosskeys)])), tl(winlosskeys), winners)
+    end
+  end
+
+  def addwin(entry) do
+    [Enum.at(entry, 0) + 1, Enum.at(entry, 1)]
+  end
+
+  def addloss(entry) do
+    [Enum.at(entry, 0), Enum.at(entry, 1) + 1]
+  end
+
+  def anywinners(guesses) do
+    Enum.map(Enum.filter(guesses, fn x -> Enum.at(x, 2) == 4 end), fn x -> Enum.at(x, 0) end)
+  end
+
+  def extractguesses(currguesses) do
+    extractguesshelp(currguesses, Map.keys(currguesses), [])
+  end
+
+  def extractguesshelp(currguesses, keys, guesses) do
+    if empty?(keys) do
+      guesses
+    else
+      extractguesshelp(currguesses, tl(keys), [[hd(keys), currguesses[hd(keys)]] | guesses])
+    end
+  end
+
+  # Note that the given player has decided to pass this round
+  # If they had guessed, remove that guess
+  def pass(gamestate, name) do
+    gamestate = Map.put(gamestate, :passed, [name | gamestate.passed])
+    Map.put(gamestate, :currentguesses, Map.drop(gamestate.currentguesses, [name]))
+  end
+
+  # Add a player into the game. If it's not in setup phase, add them as an observer.
+  # If it is in setup phase, let them in as a player.
+  def addplayer(gamestate, name, winloss) do
+    Map.put(gamestate, :playerswinloss, Map.put(gamestate.playerswinloss, name, winloss))
+  end
+
+  # Remove a player from the game, either as an observer or player. They won't
+  # be in the game anymore, so only the record of their guesses will stay.
+  # Push the wins and losses they made in this round to the server?
+  def removeplayer(gamestate, name) do
+    Map.put(gamestate, :playerswinloss, Map.drop(gamestate.playerswinloss, [name]))
+    |> Map.put(:currentguesses, Map.drop(gamestate.currentguesses, [name]))
+    |> Map.put(:playersready, List.delete(gamestate.playersready, name))
+    |> Map.put(:winners, List.delete(gamestate.winners, name))
+    |> Map.put(:passed, List.delete(gamestate.passed, name))
+  end
+
+  def getwinloss(gamestate, name) do
+    gamestate.playerswinloss[name]
+  end
+
+  # Set the player to ready, indicating that they're ready for the game to start.
+  def readyplayer(gamestate, name) do
+    if gamestate.setup do
+      Map.put(gamestate, :playersready, Enum.dedup([name | gamestate.playersready]))
+    else
+      gamestate
+    end
+
+  end
+
+  # Set the player to not ready, indicating that they're not ready for the game to start.
+  def unreadyplayer(gamestate, name) do
+    if gamestate.setup do
+      Map.put(gamestate, :playersready, List.delete(gamestate.playersready, name))
+    else
+      gamestate
+    end
+  end
+
+  # Create a random list of 4 digits
+  def random_digits() do
+    get_digits(4, [], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  end
+
+  # Helper for random_digits with an accumulator of the secret being built and the remaining uniq numbers
+  def get_digits(numdigits, digits, avail) do
+    cond do
+      numdigits <= 0 -> digits
+      numdigits > 0 ->
+        {adddigit, newavail} = List.pop_at(avail, :rand.uniform(Enum.count(avail) - 1))
+        get_digits(numdigits - 1, [adddigit | digits], newavail)
+    end
+  end
+
+  # Take a list of guesses and make a list of cows and bulls for the given secret
+  def all_cows_bulls(guesses, digits) do
+    Enum.map(guesses, fn guess -> Enum.concat(guess, cows_bulls(Enum.at(guess, 1), digits)) end)
+  end
+
+  # Takes a guess and gives the number of cows and bulls for the guess
+  def cows_bulls(guess, digits) do
+    cows_bulls(guess, digits, digits, 0, 0)
+  end
+
+  # Helper for cows_bulls with accumulators to keep track of numbers of cows and bulls
+  def cows_bulls(guessrem, digitsrem, digits, cows, bulls) do
+    cond do
+      Enum.empty?(guessrem) -> [bulls, cows]
+      hd(guessrem) == hd(digitsrem) -> cows_bulls(tl(guessrem), tl(digitsrem), digits, cows, bulls + 1)
+      Enum.member?(digits, hd(guessrem)) -> cows_bulls(tl(guessrem), tl(digitsrem), digits, cows + 1, bulls)
+      true -> cows_bulls(tl(guessrem), tl(digitsrem), digits, cows, bulls)
+    end
+  end
+end
---
+++ test/multibulls/multibulls_test.exs
@@ -0,0 +1,273 @@
+defmodule Multibulls.MultibullsTest do
+  use ExUnit.Case
+  import Multibulls.Game
+
+  def getexample() do
+    %{
+      guesses: [
+        ["player1", [1, 2, 3, 4], 0, 0],
+        ["player2", [5, 6, 7, 8], 2, 0],
+        ["player3", [1, 6, 2, 9], 1, 0],
+        ["player4", [5, 3, 2, 1], 0, 0]
+      ],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{
+        "player3" => [7, 8, 9, 0],
+        "player4" => [8, 0, 7, 9]
+      },
+      passed: ["player2", "player1"],
+      winners: [],
+      playerswinloss: %{
+        "player1" => [1, 18],
+        "player2" => [4, 7],
+        "player3" => [2, 12],
+        "player4" => [5, 10],
+      },
+      playersready: ["player1", "player2", "player3", "player4"],
+      turnnumber: 2,
+      setup: false
+    }
+  end
+
+  def nowinexample() do
+    %{
+      guesses: [
+        ["player1", [1, 2, 3, 4], 0, 0],
+        ["player2", [5, 6, 7, 8], 2, 0],
+        ["player3", [1, 6, 2, 9], 1, 0],
+        ["player4", [5, 3, 2, 1], 0, 0]
+      ],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{
+        "player3" => [9, 6, 7, 5],
+        "player4" => [8, 0, 7, 9]
+      },
+      passed: ["player2", "player1"],
+      winners: [],
+      playerswinloss: %{
+        "player1" => [1, 18],
+        "player2" => [4, 7],
+        "player3" => [2, 12],
+        "player4" => [5, 10],
+      },
+      playersready: ["player1", "player2", "player3", "player4"],
+      turnnumber: 2,
+      setup: false
+    }
+  end
+
+  def setupex() do
+    %{
+      guesses: [],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{},
+      passed: [],
+      winners: [],
+      playerswinloss: %{
+        "player1" => [1, 18],
+        "player2" => [4, 7],
+        "player3" => [2, 12],
+        "player4" => [5, 10],
+      },
+      playersready: ["player1", "player2", "player3"],
+      turnnumber: 0,
+      setup: true
+    }
+  end
+
+  def lesspasses() do
+    %{
+      guesses: [
+        ["player1", [1, 2, 3, 4], 0, 0],
+        ["player2", [5, 6, 7, 8], 2, 0],
+        ["player3", [1, 6, 2, 9], 1, 0],
+        ["player4", [5, 3, 2, 1], 0, 0]
+      ],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{
+        "player3" => [7, 8, 9, 0],
+        "player4" => [8, 0, 7, 9]
+      },
+      passed: ["player1"],
+      winners: [],
+      playerswinloss: %{
+        "player1" => [1, 18],
+        "player2" => [4, 7],
+        "player3" => [2, 12],
+        "player4" => [5, 10],
+      },
+      playersready: ["player1", "player2", "player3", "player4"],
+      turnnumber: 2,
+      setup: false
+    }
+  end
+
+  def lessguesses() do
+    %{
+      guesses: [
+        ["player1", [1, 2, 3, 4], 0, 0],
+        ["player2", [5, 6, 7, 8], 2, 0],
+        ["player3", [1, 6, 2, 9], 1, 0],
+        ["player4", [5, 3, 2, 1], 0, 0]
+      ],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{
+        "player3" => [7, 8, 9, 0]
+      },
+      passed: ["player1", "player2"],
+      winners: [],
+      playerswinloss: %{
+        "player1" => [1, 18],
+        "player2" => [4, 7],
+        "player3" => [2, 12],
+        "player4" => [5, 10],
+      },
+      playersready: ["player1", "player2", "player3", "player4"],
+      turnnumber: 2,
+      setup: false
+    }
+  end
+
+  def playeroneeverywhere do
+    %{
+      guesses: ["player1", [1, 2, 3, 4], 0, 0],
+      secret: [3, 4, 5, 6],
+      currentguesses: %{"player1" => [5, 6, 7, 8]},
+      passed: ["player1"],
+      winners: ["player1"],
+      playerswinloss: %{"player1" => [1, 1]},
+      playersready: ["player1"],
+      turnumber: 0,
+      setup: true
+    }
+  end
+  # This test involves randomness so it must be human verified, so I don't run it every time.
+  #test "startnewgame" do
+  #  assert false == startnewgame(getexample())
+  #end
+
+  test "test make a guess" do
+    assert makeguess(getexample(), "player1", [8, 7, 9, 0]).currentguesses ==
+    %{
+      "player1" => [8, 7, 9, 0],
+      "player3" => [7, 8, 9, 0],
+      "player4" => [8, 0, 7, 9]
+    }
+
+    assert makeguess(getexample(), "player1", [8, 7, 9, 0]).passed == ["player2"]
+    assert makeguess(getexample(), "player1", [8, 7, 9, 0]).turnnumber == 2
+  end
+
+  test "test is valid guess" do
+      assert isValidGuess([1, 2, 3, 4])
+      assert not isValidGuess([1, 2, 3])
+      assert not isValidGuess([1, 1, 3, 4])
+  end
+
+  test "test all players guessed" do
+    assert allplayersguessed(getexample())
+    assert not allplayersguessed(lessguesses())
+    assert not allplayersguessed(lesspasses())
+  end
+
+  test "pushguesses" do
+    # Someone wins, so the game goes back to setup
+    assert %{
+      guesses: [
+        ["player1", [1, 2, 3, 4], 0, 0],
+        ["player2", [5, 6, 7, 8], 2, 0],
+        ["player3", [1, 6, 2, 9], 1, 0],
+        ["player4", [5, 3, 2, 1], 0, 0],
+        ["player4", [8, 0, 7, 9], 0, 4],
+        ["player3", [7, 8, 9, 0], 4, 0]
+      ],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{},
+      passed: [],
+      winners: ["player3"],
+      playerswinloss: %{
+        "player1" => [1, 19],
+        "player2" => [4, 8],
+        "player3" => [3, 12],
+        "player4" => [5, 11],
+      },
+      playersready: [],
+      turnnumber: 3,
+      setup: true
+    } == pushguesses(getexample())
+
+    # None one wins, so the game is just progressed
+    assert %{
+      guesses: [
+        ["player1", [1, 2, 3, 4], 0, 0],
+        ["player2", [5, 6, 7, 8], 2, 0],
+        ["player3", [1, 6, 2, 9], 1, 0],
+        ["player4", [5, 3, 2, 1], 0, 0],
+        ["player4", [8, 0, 7, 9], 0, 4],
+        ["player3", [9, 6, 7, 5], 0, 2]
+      ],
+      secret: [7, 8, 9, 0],
+      currentguesses: %{},
+      passed: [],
+      winners: [],
+      playerswinloss: %{
+        "player1" => [1, 18],
+        "player2" => [4, 7],
+        "player3" => [2, 12],
+        "player4" => [5, 10],
+      },
+      playersready: ["player1", "player2", "player3", "player4"],
+      turnnumber: 3,
+      setup: false
+    } == pushguesses(nowinexample())
+  end
+
+  test "extractguesses" do
+    assert Enum.sort(extractguesses(%{"player3" => [7, 8, 9, 0],"player4" => [8, 0, 7, 9]}))
+      == Enum.sort([["player3", [7, 8, 9, 0]], ["player4", [8, 0, 7, 9]]])
+  end
+
+  test "test pass" do
+    assert getexample() == pass(lesspasses(), "player2")
+    assert pass(getexample(), "player3").currentguesses ==
+    %{
+      "player4" => [8, 0, 7, 9]
+    }
+    assert Enum.sort(pass(getexample(), "player3").passed) == ["player1", "player2", "player3"]
+  end
+
+  test "test addplayer" do
+    assert addplayer(getexample(), "player5", [0, 0]).playerswinloss ==
+    %{
+      "player1" => [1, 18],
+      "player2" => [4, 7],
+      "player3" => [2, 12],
+      "player4" => [5, 10],
+      "player5" => [0, 0]
+    }
+    assert addplayer(getexample(), "player5", [0, 0]).playerswinloss
+  end
+
+  test "removeplayer" do
+    assert removeplayer(playeroneeverywhere(), "player1") ==
+    Map.put(Map.put(new(), :guesses, ["player1", [1, 2, 3, 4], 0, 0]), :secret, [3, 4, 5, 6])
+  end
+
+  test "getwinloss" do
+    assert getwinloss(getexample(), "player1") == [1, 18]
+    assert getwinloss(getexample(), "player2") == [4, 7]
+    assert getwinloss(getexample(), "player3") == [2, 12]
+    assert getwinloss(getexample(), "player4") == [5, 10]
+  end
+
+  test "readyplayer" do
+    assert readyplayer(getexample(), "player4") == getexample()
+    assert Enum.sort(readyplayer(setupex(), "player4").playersready) ==
+      Enum.sort(["player1", "player2", "player3", "player4"])
+  end
+
+  test "unreadyplayer" do
+    assert unreadyplayer(getexample(), "player1") == getexample()
+    assert unreadyplayer(setupex(), "player1").playersready == ["player2", "player3"]
+  end
+end
